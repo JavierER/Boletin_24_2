f2c22538612ccc5e098a1e67402a9b7102b57729
Usuario <Usuario@192.168.0.154>
2016-06-10 23:08:38 +0200

    Finalisado

---
+++ src/boletin_24/pkg2/Boletin_242.java
@@ -0,0 +1,15 @@
+package boletin_24.pkg2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Boletin_242 {
+
+    
+    public static void main(String[] args) {
+       Frame window= new Frame();
+       window.setVisible(true);
+    }
+    
+}
